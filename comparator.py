312b03f1308dff7159e474b18d40c97d61ca3a60
Nouble Sameem <nouble_a@ndvor.com>
2020-09-24 17:23:24 +0530

    Add timeout variable to config file and change all references in comparator.py

--- comparator.py
+++ comparator.py
@@ -19,21 +19,22 @@ class comparator():
         self.VARIANCE_TOLERANCE_LIMIT = config['VARIANCE_TOLERANCE_LIMIT']
         self.TEMPERATURE_UNIT = config['TEMPERATURE_UNIT']
         self.API_KEY = config['API_KEY']
+        self.BROWSER_TIMEOUT = config['BROWSER_TIMEOUT']
 
     def _get_temperature_from_ui(self, city_name):
         page = weatherPage(self.BROWSER)
         page.go_to_ndtv_homepage()
         page.click_three_dots_icon()
         page.navigate_to_weather_page()
-        time.sleep(3)
+        time.sleep(self.BROWSER_TIMEOUT)
 
         page.enter_text_in_searchbox(city_name)
-        time.sleep(3)
+        time.sleep(self.BROWSER_TIMEOUT)
         if not page.is_city_selected(city_name):
             page.select_city(city_name)
-        time.sleep(3)
+        time.sleep(self.BROWSER_TIMEOUT)
         page.click_city_on_map(city_name)
-        time.sleep(3)
+        time.sleep(self.BROWSER_TIMEOUT)
         conditions = page.get_weather_conditions(city_name)
 
         return conditions['Temp in Degrees']
