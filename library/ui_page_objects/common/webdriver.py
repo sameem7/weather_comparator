ba70f8b38e9a7373f60581c9eef925ed9368ac46
Nouble Sameem <nouble_a@ndvor.com>
2020-09-24 19:00:55 +0530

    Add error handling to all functions

--- library/ui_page_objects/common/webdriver.py
+++ library/ui_page_objects/common/webdriver.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
+
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
@@ -19,24 +21,32 @@ class Driver(webdriver.Chrome):
             print('Browser is already closed.')
     
     def enter_text(self, text, locator):
-        self.driver.find_element_by_id(locator).send_keys(text)
+        try:
+            self.driver.find_element_by_id(locator).send_keys(text)
+        except ElementNotSelectableException as not_selectable:
+            print('Element {0} is not selectable'.format(not_selectable))
+        except ElementNotVisibleException as not_visible:
+            print('Element {0} is not visible'.format(not_visible))
+        except ElementNotInteractableException as not_interactable:
+            print('Element {0} is not interactable'.format(not_interactable))    
 
     def is_selected(self, locator):
-        return self.driver.find_element_by_id(locator).is_selected()
+        try:
+            return self.driver.find_element_by_id(locator).is_selected()
+        except ElementNotSelectableException as not_selectable:
+            print('Element {0} is not selectable'.format(not_selectable))    
 
     def click_element(self, locator, by='id'):
-        if by.lower() == 'id':
-            self.driver.find_element_by_id(locator).click()
-        elif by.lower() == 'xpath':
-            self.driver.find_element_by_xpath(locator).click()
+        try:
+            if by.lower() == 'id':
+                self.driver.find_element_by_id(locator).click()
+            elif by.lower() == 'xpath':
+                self.driver.find_element_by_xpath(locator).click()
+        except ElementNotSelectableException as not_selectable:
+            print('Element {0} is not selectable'.format(not_selectable))    
 
     def get_content(self, locator, by='id'):
         if by.lower() == 'id':
             self.driver.find_element_by_id(locator).text
         elif by.lower() == 'xpath':
             self.driver.find_element_by_xpath(locator).text
-        
-class Wait(webdriver.support.ui.WebDriverWait):
-
-    def wait_until_element_is_visible(self):
-        pass
